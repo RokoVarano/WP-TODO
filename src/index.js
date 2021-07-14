f4b698dc42612dc9ef2652ca709b8609afe0526e
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-07-14 16:39:50 -0400

    Add 'edit task' functionality

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import './style.scss';
 import createList from './modules/frontend/listItems';
+import { loadTasks } from './modules/backend/task';
 
 const run = () => {
-  let load = JSON.parse(localStorage.getItem('tasks'));
-
-  if (load == null) {
-    load = [];
-  }
-
-  createList(load);
+  createList(loadTasks());
 };
 
 run();
